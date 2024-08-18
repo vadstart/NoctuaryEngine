c59cce27af1381ce5a841c4a3787f11a03e11f78
vstovbunskyi <vaderxv@gmail.com>
2024-08-18 21:03:33 +0200

    FINALLY A TRIANGLE!

--- src/astral_app.cpp
+++ src/astral_app.cpp
@@ -1,5 +1,7 @@
 #include "astral_app.h"
 
+#include <array>
+#include <cstdint>
 #include <stdexcept>
 
 namespace nt
@@ -18,7 +20,10 @@ void AstralApp::run() {
 
   while (!ntWindow.shouldClose()) {
     glfwPollEvents();
+    drawFrame();
   }
+
+  vkDeviceWaitIdle(ntDevice.device());
 }
 
  void AstralApp::createPipelineLayout() {
@@ -46,8 +51,69 @@ void AstralApp::run() {
         "src/shaders/simple_shader.frag.spv");
   }
 
-void AstralApp::createCommandBuffers() {}
-void AstralApp::drawFrame() {}
+void AstralApp::createCommandBuffers() {
+  commandBuffers.resize(ntSwapChain.imageCount());
+
+  VkCommandBufferAllocateInfo allocInfo{};
+  allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  allocInfo.commandPool = ntDevice.getCommandPool();
+  allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+  if (vkAllocateCommandBuffers(ntDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
+    throw std::runtime_error("failed to allocate command buffers!");
+  }
+
+  for (int i = 0; i < commandBuffers.size(); i++) {
+    VkCommandBufferBeginInfo beginInfo{};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+    if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+      throw std::runtime_error("failed to begin recording command buffer!");
+    }
+    
+    VkRenderPassBeginInfo renderPassInfo{};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassInfo.renderPass = ntSwapChain.getRenderPass();
+    renderPassInfo.framebuffer = ntSwapChain.getFrameBuffer(i);
+
+    renderPassInfo.renderArea.offset = {0, 0};
+    renderPassInfo.renderArea.extent = ntSwapChain.getSwapChainExtent();
+
+    std::array<VkClearValue, 2> clearValues{};
+    clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+    clearValues[1].depthStencil = {1.0f, 0};
+    //uint32_t clearValueCount = static_cast<uint32_t>(clearValues.size());
+    //renderPassInfo.clearValueCount = clearValueCount;    
+    renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+    renderPassInfo.pClearValues = clearValues.data();
+
+    vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+    ntPipeline->bind(commandBuffers[i]);
+    vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+    vkCmdEndRenderPass(commandBuffers[i]);
+    if(vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+      throw std::runtime_error("failed to record command buffer!");
+    }
+  }
+
+}
+
+void AstralApp::drawFrame() {
+  uint32_t imageIndex;
+  auto result = ntSwapChain.acquireNextImage(&imageIndex);
+
+  if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+    throw std::runtime_error("failed to acquire swap chain image!");
+  }
+
+  result = ntSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+  if (result != VK_SUCCESS) {
+    throw std::runtime_error("failed to present swap chain image!");
+  }
+}
 
 
 }
--- src/nt_pipeline.h
+++ src/nt_pipeline.h
@@ -35,6 +35,8 @@ namespace nt {
       NtPipeline(const NtPipeline&) = delete;
       void operator=(const NtPipeline&) = delete;
 
+      void bind(VkCommandBuffer commandBuffer);
+
       static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
 
     private:
