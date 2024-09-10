ed6c91155e6d8562defe665bbb007b072dc56d71
vstovbunskyi <vaderxv@gmail.com>
2024-09-10 19:14:46 +0200

    Added 'compile_commands.json' generation for Vulkan/GLWF/GLM support by LSP;

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe src\shaders\simple_shader.vert -o src\shaders\simple_shader.vert.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe src\shaders\simple_shader.frag -o src\shaders\simple_shader.frag.spv
+C:\VulkanSDK\1.3.290.0\Bin\glslc.exe src\shaders\simple_shader.vert -o build\shaders\simple_shader.vert.spv
+C:\VulkanSDK\1.3.290.0\Bin\glslc.exe src\shaders\simple_shader.frag -o build\shaders\simple_shader.frag.spv
 pause
